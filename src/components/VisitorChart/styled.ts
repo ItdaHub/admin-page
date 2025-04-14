05190a663cd486fd32ddb5d26f81cd582c5f0886
Taetae1123 <xodus2079@naver.com>
2025-04-14 17:04:54 +0900

    Fix: 대시보드에 chart 추가

---
+++ src/components/VisitorChart/styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const VisitorChartStyled = styled.div`
+  &.visitor-wrap {
+    .visitor-title {
+      margin: 25px 0;
+      font-weight: 700;
+      font-size: 17px;
+    }
+    .visitor-chart {
+      margin-top: 40px;
+    }
+  }
+`;
--- src/features/DashPage/index.tsx
+++ src/features/DashPage/index.tsx
@@ -1,10 +1,12 @@
 import TitleCompo from "@/components/TitleCompo";
 import { DashStyled } from "./styled";
+import VisitorChart from "@/components/VisitorChart";
 
 const DashBoard = () => {
   return (
     <DashStyled>
       <TitleCompo title="대시보드" />
+      <VisitorChart />
     </DashStyled>
   );
 };
