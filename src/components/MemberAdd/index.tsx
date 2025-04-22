9adda3f85cf5fc67ee55932fd78b48a401fd5b4c
Taetae1123 <xodus2079@naver.com>
2025-04-22 21:49:06 +0900

    Fix: 회원수정 body에 알맞은 값담아서 요청 보내기

--- src/components/MemberAdd/index.tsx
+++ src/components/MemberAdd/index.tsx
@@ -58,7 +58,7 @@ const MemberAdd = ({ id }: { id?: number }) => {
         message.info("이메일과 닉네임 중복검사를 완료해주세요.");
         return;
       }
-      const { passwordCheck, ...filteredValues } = values;
+      const { passwordCheck, birthYear, ...filteredValues } = values;
 
       console.log("뭐가 들었나", filteredValues);
 
